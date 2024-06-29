9c98e823d72514a37b8c434826f858a304383470
Jared Goodwin <translucency@outlook.com>
2024-06-29 11:39:51 -0700

    Remove old mapping.

--- Bitbound.WebsocketBridge/Program.cs
+++ Bitbound.WebsocketBridge/Program.cs
@@ -8,20 +8,6 @@ var app = builder.Build();
 
 var api = app.MapGroup("/api");
 api.MapGet("/status", () => Results.NoContent());
-
-api.MapGet("/bridge/{id}", async (Guid id, HttpContext context) =>
-{
-    if (!context.WebSockets.IsWebSocketRequest)
-    {
-        return Results.BadRequest();
-    }
-
-    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-    
-
-    return Results.Ok();
-});
-
 app.UseMiddleware<WebsocketBridgeMiddleware>();
 
 app.Run();
