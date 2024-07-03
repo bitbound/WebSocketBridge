914e388570c463cfb04cd87912d28405dbc1d45a
Jared Goodwin <translucency@outlook.com>
2024-07-03 06:16:03 -0700

    Add healthchecks instead of status.

--- Bitbound.WebsocketBridge/Program.cs
+++ Bitbound.WebsocketBridge/Program.cs
@@ -1,5 +1,6 @@
 using Bitbound.WebsocketBridge.Dtos;
 using Bitbound.WebsocketBridge.Serialization;
+using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.AspNetCore.WebSockets;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -10,11 +11,12 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 });
 
 builder.Services.AddWebSockets(_ => { });
+builder.Services.AddHealthChecks();
 builder.Services.AddSingleton<ISessionStore, SessionStore>();
 var app = builder.Build();
 
 var api = app.MapGroup("/api");
-api.MapGet("/status", () => Results.Ok(new StatusOkDto("Bridge is running!")));
+api.MapHealthChecks("/health");
 
 app.UseWhen(x => x.Request.Path.StartsWithSegments("/bridge"), x =>
 {
